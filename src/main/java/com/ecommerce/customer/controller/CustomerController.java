3e086a9f2118442f93245e7a19643c8eb457056e
ibhale <ishanbhale1989@gmail.com>
2021-05-05 01:24:49 -0500

    added swagger and lombok

--- src/main/java/com/ecommerce/customer/controller/CustomerController.java
+++ src/main/java/com/ecommerce/customer/controller/CustomerController.java
@@ -1,16 +1,20 @@
 package com.ecommerce.customer.controller;
 
+import com.ecommerce.customer.model.Customer;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class CustomerController {
 
-    @GetMapping("/")
-    public ResponseEntity<?> greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+    @GetMapping("/test")
+    public ResponseEntity<?> test(@RequestParam(value = "name", defaultValue = "World") String name) {
         return new ResponseEntity<>("Hello " + name, HttpStatus.OK);
+    }
+
+    @PostMapping("/customers/{customerId}")
+    public ResponseEntity<?> addCustomer(@RequestBody Customer customer){
+        return new ResponseEntity<>("Customer added-" +  customer.getCustomerName(), HttpStatus.CREATED);
     }
 }
---
+++ src/main/java/com/ecommerce/customer/model/Customer.java
@@ -0,0 +1,9 @@
+package com.ecommerce.customer.model;
+
+import lombok.Data;
+
+@Data
+public class Customer {
+    private String customerId;
+    private String customerName;
+}
